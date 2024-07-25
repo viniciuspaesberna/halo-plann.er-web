ea9f0cdc130e373fd5ea7d2065fa5363eb87f354
Vinicius Paes Berna <vini_berna2.0@hotmail.com>
2024-07-25 13:58:45 -0300

    feat: add create link functionality

---
+++ src/pages/trip-details/create-link-modals.tsx
@@ -0,0 +1,99 @@
+import { useForm } from "react-hook-form";
+import { Modal } from "../../components/modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Link2, Tag } from "lucide-react";
+import { Button } from "../../components/button";
+import { api } from "../../lib/axios";
+import { useState } from "react";
+import { useTripDetails } from "../../contexts/trip-details-context";
+
+interface CreateLinkModalProps {
+  isOpen: boolean
+  onClose: () => void
+}
+
+const createLinkFormSchema = z.object({
+  title: z.string({ required_error: "Título é obrigatório" }).min(3, { message: "Título deve conter pelo menos 3 letras" }),
+  url: z.string({ required_error: "URL é obrigatório" }).url({ message: "Formato de URL incorreto" })
+})
+
+type CreateLinkFormData = z.infer<typeof createLinkFormSchema>
+
+export const CreateLinkModal = ({
+  isOpen,
+  onClose
+}: CreateLinkModalProps) => {
+  const { tripId } = useTripDetails()
+
+  const [isLoading, setIsLoading] = useState(false)
+  const { handleSubmit, register, formState: { errors } } = useForm<CreateLinkFormData>({
+    resolver: zodResolver(createLinkFormSchema)
+  })
+
+  async function createLink(data: CreateLinkFormData) {
+    const { title, url } = data
+
+    setIsLoading(true)
+
+    console.log(title, url)
+
+    await api.post(`/trips/${tripId}/links`, {
+      title,
+      url
+    }).then(() => {
+      setIsLoading(false)
+      window.document.location.reload()
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  return (
+    <Modal
+      heading="Cadastrar novo link"
+      description="Todos os convidados poderão acessar esse link."
+      isOpen={isOpen}
+      onClose={onClose}
+      className="max-w-[640px] w-full"
+    >
+      <form onSubmit={handleSubmit(createLink)} className="space-y-2">
+        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-4">
+          <Tag className="size-5 ml-2 text-zinc-400" />
+
+          <input
+            placeholder="Título do link"
+            className="flex-1 bg-transparent text-lg placeholder-zinc-400 outline-none"
+            {...register('title')}
+          />
+
+          {errors.title && (
+            <small className="text-zinc-400 text-sm">
+              {errors.title.message}
+            </small>
+          )}
+        </div>
+
+        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-4">
+          <Link2 className="size-5 ml-2 text-zinc-400" />
+
+          <input
+            placeholder="URL"
+            className="flex-1 bg-transparent text-lg placeholder-zinc-400 outline-none"
+            {...register('url')}
+          />
+
+          {errors.url && (
+            <small className="text-zinc-400 text-sm">
+              {errors.url.message}
+            </small>
+          )}
+        </div>
+
+        <Button type="submit" size="full" isLoading={isLoading}>
+          Salvar link
+        </Button>
+      </form>
+    </Modal>
+  );
+}
--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,35 +1,58 @@
+import { useEffect, useState } from "react";
+
 import { Link2, Plus } from "lucide-react";
+
+import { api } from "../../lib/axios";
 import { Button } from "../../components/button";
+import { useTripDetails } from "../../contexts/trip-details-context";
+import { CreateLinkModal } from "./create-link-modals";
+
+
+type Link = {
+  id: string;
+  title: string;
+  url: string;
+}
 
 export const ImportantLinks = () => {
+  const [isCreateLinksModalOpen, setIsCreateLinksModalOpen] = useState(false)
+  const [links, setLinks] = useState<Link[]>([])
+
+  const { tripId } = useTripDetails()
+
+  useEffect(() => {
+    api.get<{ links: Link[] }>(`/trips/${tripId}/links`).then(response => setLinks(response.data.links))
+  }, [tripId])
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
 
       <div className="space-y-5">
-        <a target="_blank" href="https://airbnb.com" className="flex gap-4 group items-center justify-between">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-            <span className="block text-xs text-zinc-400 truncate group-hover:text-zinc-200">https://airbnb.com/asdfasdfasdfasdfasdfasdfasdf12341234123412341234</span>
-          </div>
+        {links.map(link => (
+          <a key={link.id} target="_blank" href={link.url} className="flex gap-4 group items-baseline justify-between">
+            <div className="space-y-1.5">
+              <span className="block font-medium text-zinc-100">{link.title}</span>
+              <span className="block text-xs text-zinc-400 truncate group-hover:text-zinc-200">{link.url}</span>
+            </div>
 
-          <Link2 className="shrink-0 text-zinc-400 size-5 group-hover:text-zinc-200" />
-        </a>
+            <Link2 className="shrink-0 text-zinc-400 size-5 group-hover:text-zinc-200" />
+          </a>
+        ))}
 
-        <a target="_blank" href="https://airbnb.com" className="flex gap-4 group items-center justify-between">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-            <span className="block text-xs text-zinc-400 truncate group-hover:text-zinc-200">https://airbnb.com/asdfasdfasdfasdfasdfasdfasdf12341234123412341234</span>
-          </div>
-
-          <Link2 className="shrink-0 text-zinc-400 size-5 group-hover:text-zinc-200" />
-        </a>
+        {links.length === 0 && (
+          <span className="text-zinc-400 w-full flex items-center justify-center gap-2">
+            Cadastre links importantes sobre a viagem.
+          </span>
+        )}
       </div>
 
-      <Button variant="secondary" size="full">
+      <Button onClick={() => setIsCreateLinksModalOpen(true)} variant="secondary" size="full">
         <Plus className="size-5" />
         Cadastrar novo link
       </Button>
+
+      <CreateLinkModal isOpen={isCreateLinksModalOpen} onClose={() => setIsCreateLinksModalOpen(false)} />
     </div>
   );
 }
