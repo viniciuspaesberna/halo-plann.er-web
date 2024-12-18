16fdcc109e66194b23a1abb8007aa4caec9be745
Vinicius Paes Berna <viniciuspaesberna@gmail.com>
2024-12-18 10:27:51 -0300

    fix: expose api base url environment variable to client

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiBaseUrl = import.meta.env.API_BASE_URL
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
 
 export const api = axios.create({
   baseURL: apiBaseUrl || 'http://localhost:3333',
